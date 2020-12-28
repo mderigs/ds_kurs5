fe3249416021b042a15357fc12c604c87885e19e
Martin Derigs <martin@derigs.de>
2020-12-28 19:12:24 +0100

    Dateisystemkommandos

--- test.R
+++ test.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+library(dslabs)
+data(murders)
+
+mrate <- murders %>% mutate(region = factor(region), rate = total / population*10^5) %>%
+  mutate(abb = reorder(abb, rate))
+mrate %>%
+  ggplot(aes(abb, rate)) +
+  geom_bar(width = 0.5, stat = "identity", color = "black") +
+  coord_flip()
+
+getwd()
+save(mrate, file = "rda/mrate.rda")
+ggsave("figs/barplot.png")
